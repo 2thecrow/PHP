ee49c077183eabf7287e365ce9ce677a7d13c666
2thecrow <pletnev1990.a.a@gmail.com>
2024-08-12 03:52:38 +0200

    lesson 1

---
+++ Playlist/ITDoctor/lesson 1/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Compressed CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites@6.8.1/dist/css/foundation.min.css" crossorigin="anonymous">
+
+    <!-- Compressed JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/foundation-sites@6.8.1/dist/js/foundation.min.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <main class="grid-x">
+        123
+    </main>
+</body>
+
+</html>
---
+++ Playlist/ITDoctor/readme.md
@@ -0,0 +1,2 @@
+ITDoctor - PHP Playlist
+https://www.youtube.com/watch?v=vrxDEAGGL60&list=PLuY6eeDuleIN_pFzp1vlu0PD3KXUrPTVS
